074994c0a9111d23c0583eb0caaf2b9acaad3a4c
sammy <sackah08@gmail.com>
2024-12-01 23:46:27 -0800

    second-lambda4

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handle(event, context):
     return{
         'statusCode': 200,
-        'body': json.dumps('Hello again from cicd-test lambda from vscode')
+        'body': json.dumps('Hello1 again from cicd-test lambda from vscode')
     }
